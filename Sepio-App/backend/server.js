92d50309add9671fc808a58e3612cec75ad8d3dd
Your Name <you@example.com>
2024-07-13 07:33:07 +0300

    test

--- Sepio-App/backend/server.js
+++ Sepio-App/backend/server.js
@@ -1655,7 +1655,7 @@ async function createAdminUser() {
   try {
     // Check if the admin user already exists
     const existingAdmin = await prisma.user.findUnique({
-      where: { name: 'admin' },
+      where: { name: 'adminyhjuyjhuyh' },
     });
 
     // If the admin user does not exist, create it
@@ -1665,7 +1665,7 @@ async function createAdminUser() {
 
       const newAdmin = await prisma.user.create({
         data: {
-          name: 'admin',
+          name: 'last',
           password: hashedPassword,
           privileges: 'ADMIN',
           otp_secret: '',
